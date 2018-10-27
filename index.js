b95e4471dbe209460c6ac672a6338a7ab5ef4e31
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-10-27 17:50:24 +0200

    api

--- index.js
+++ index.js
@@ -12,16 +12,29 @@ console.log(
   )
 );
 
-
 async function startDocker() {
   // delete the bch-regtest
-  console.log("Restarting docker container");
+  console.log("Restarting Bitcoin Cash Client");
 
   const deletedDocker = await exec('docker rm bch-regtest -f');
   
   exec('docker run --name bch-regtest -p 18332:18332 slashrsm/bitcoin-cash-regtest:latest &');
 
-  console.log("Docker container restarted");
+  console.log("Bitcoin Cash Client restarted and listens at port 18332");
 }
 
-startDocker();
+async function startBitboxApi() {
+    // delete the bch-regtest
+    console.log("Starting BITBOX Api listens at port 3000");
+  
+    const api = await exec('BITCOINCOM_BASEURL=http://localhost:3000/api/ RPC_BASEURL=http://127.0.1.0:18332/ RPC_PASSWORD=regtest RPC_USERNAME=regtest ZEROMQ_PORT=0 ZEROMQ_URL=0 NETWORK=local node ./node_modules/rest.bitcoin.com/app.js');
+    
+    console.log(api);
+}
+
+startDocker()
+.then(() => {
+    startBitboxApi();
+})
+
+
